6eeb04f5260c10842b485c7bf8e89ee9855bb67e
zerofancy <ntutn.top@gmail.com>
2024-09-15 00:05:07 +0800

    网页按键支持

--- composeApp/src/wasmJsMain/kotlin/top/ntutn/tetris/main.kt
+++ composeApp/src/wasmJsMain/kotlin/top/ntutn/tetris/main.kt
@@ -1,12 +1,36 @@
 package top.ntutn.tetris
 
+import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.w3c.dom.events.KeyboardEvent
+import top.ntutn.tetris.input.IInputHandler
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        var inputHandler by remember { mutableStateOf<IInputHandler?>(null) }
+
+        LaunchedEffect(document) {
+            document.addEventListener("keyup", { event ->
+                when ((event as? KeyboardEvent)?.key) {
+                    "w", "ArrowUp" -> inputHandler?.top() ?: false
+                    "s", "ArrowDown" -> inputHandler?.bottom() ?: false
+                    "a", "ArrowLeft" -> inputHandler?.left() ?: false
+                    "d", "ArrowRight" -> inputHandler?.right() ?: false
+                    " " -> inputHandler?.pause() ?: false
+                    "Enter" -> inputHandler?.select() ?: false
+                    "Escape" -> inputHandler?.cancel() ?: false
+                    else -> false
+                }
+            })
+        }
+
+        App { handler ->
+            inputHandler = handler
+        }
     }
 }
